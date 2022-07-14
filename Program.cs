866ef6159d14d679b872d0beef1fb3c4af330df8
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-14 19:03:31 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести все натуральные числа в промежутке от M до N
+
+
+void Print(int a, int b)
+{
+    if (a > b) return;
+    Console.Write(a + " ");
+    Print(a+1, b);
+}
+
+int M = 4;
+int N = 9;
+
+Print(M,N);
